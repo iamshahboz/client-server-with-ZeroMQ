fdb3cec12906a7f106766ef4420145544cfb12d6
Shahboz <sharafzodashahboz@gmail.com>
2024-07-17 22:06:34 -0700

    client side

--- client.py
+++ client.py
@@ -1,32 +1,25 @@
 import logging
-from pydantic import BaseModel
 from zmq_message import ZMQPublisher
 import time
 
+
 logging.basicConfig(level=logging.INFO)
-
 MESSAGE_SENDING_PORT = 8880
-MESSAGE_SENDING_CHANNEL = f'tcp://0.0.0.0:{MESSAGE_SENDING_PORT}'
 
-class Student(BaseModel):
-    name: str
-    age: int
-    email: str
 
 
 def run_message_publisher():
     publishing_period_secs = 1 
     publisher = ZMQPublisher(f"tcp://*:{MESSAGE_SENDING_PORT}")
     while True:
-        msg = Student(
-            name = 'Jack',
-            age = 22,
-            email= 'jacksmith@gmail.com'
-        )
+        msg = "Hello World!"
         logging.info(f'[SEND] {msg}')
         publisher.send_msg(msg)
-        time.sleep(1)
+        time.sleep(publishing_period_secs)
         
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     run_message_publisher()
+    
+
     
